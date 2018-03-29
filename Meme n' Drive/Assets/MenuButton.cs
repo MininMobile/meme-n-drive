8eb593d10dd9d28487dd7bd0671c6b8ed233300c
MininMobile <miningamer@gmail.com>
2018-03-29 20:35:20 +0100

    Create Menu Button Action

---
+++ Meme n' Drive/Assets/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour {
+    void OnMouseOver() {
+        Debug.Log(".o.");
+        transform.Translate(
+            1,
+            0,
+            0
+        );
+    }
+    void OnMouseExit() {
+        Debug.Log(";-;");
+        transform.Translate(
+            -1,
+            0,
+            0
+        );
+    }
+}
